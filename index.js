6ad16c4300e854c14ceda42253cb801e52300f2b
Stijn <stijn.brysbaert@ugent.be>
2020-10-19 10:57:45 +0200

    fixes #2

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
+const N3 = require('n3');
 var myFile = fs.createWriteStream("./public/output.ttl");
 
 const newEngine = require('@comunica/actor-init-sparql').newEngine;
 
 const myEngine = newEngine();
 
-async function get(){
-    const result = await myEngine.query(`
-    PREFIX geo: <http://www.opengis.net/ont/geosparql#>
+async function query() {
+  const result = await myEngine.query(`
     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
     PREFIX foaf: <http://xmlns.com/foaf/0.1/>
@@ -35,15 +35,28 @@ async function get(){
           ?s mv:totalCapacity ?totCap .
           ?s geonames:nearby ?nearby .
         }
-      } LIMIT 100`, {
+      }`, {
     sources: ["https://datapiloten.be/bluebike/availabilities.geojson"],
-    });
+  });
 
-    const { data } = await myEngine.resultToString(result, 'text/turtle');
-      data.pipe(myFile);
-    //   console.log(data);
-
+  const writer = new N3.Writer(myFile, { end: false, prefixes:
+    {
+      rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+      rdfs: 'http://www.w3.org/2000/01/rdf-schema#',
+      foaf: 'http://xmlns.com/foaf/0.1/',
+      mv: 'http://schema.mobivoc.org/',
+      geo: 'http://www.w3.org/2003/01/geo/wgs84_pos#',
+      locn: 'http://www.w3.org/ns/locn#',
+      tn: 'https://data.vlaanderen.be/ns/transportnetwerk#',
+      ext: 'https://stijnbrysbaert.github.io/OSLO-extension/vocabulary.ttl#',
+      geonames: 'http://www.geonames.org/ontology#',
+      xsd:      'http://www.w3.org/2001/XMLSchema#'
+    }
+  });
+  const quads = await result.quads();
+  writer.addQuads(quads);
+  writer.end();
 }
 
-get();
+query();
 
