474c32d50b887a25c0af930690f079d89d9827f5
Stijn <stijn.brysbaert@ugent.be>
2020-10-19 09:35:15 +0200

    cross origin domain changed to *

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 8080
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://query.linkeddatafragments.org"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
