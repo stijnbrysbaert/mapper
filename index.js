6525c8428c07a6c61ea3a265d0b331b32382b487
Stijn <stijn.brysbaert@ugent.be>
2020-10-19 15:33:02 +0200

    changes to sparql query

--- index.js
+++ index.js
@@ -8,24 +8,35 @@ const myEngine = newEngine();
 exports.cronjob = async () => {
   var myFile = fs.createWriteStream("./public/output.ttl");
   const result = await myEngine.query(`
-    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX adr:  <https://data.vlaanderen.be/ns/adres#>
+    PREFIX dc:   <http://purl.org/dc/terms/>
+    PREFIX ext:  <https://stijnbrysbaert.github.io/OSLO-extension/vocabulary.ttl#>
     PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-    PREFIX mv:   <http://schema.mobivoc.org/>
     PREFIX geo:  <http://www.w3.org/2003/01/geo/wgs84_pos#>
-    PREFIX locn:  <http://www.w3.org/ns/locn#> 
-    PREFIX tn:    <https://data.vlaanderen.be/ns/transportnetwerk#>
-    PREFIX ext:   <https://stijnbrysbaert.github.io/OSLO-extension/vocabulary.ttl#>
     PREFIX geonames: <http://www.geonames.org/ontology#>
+    PREFIX locn: <http://www.w3.org/ns/locn#> 
+    PREFIX mv:   <http://schema.mobivoc.org/>
+    PREFIX prov: <http://www.w3.org/ns/prov#>
+    PREFIX rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX tn:   <https://data.vlaanderen.be/ns/transportnetwerk#>
+    PREFIX trips:<https://data.vlaanderen.be/ns/mobiliteit/trips-en-aanbod#>
 
     CONSTRUCT {
-        ?s rdf:type mv:ParkingFacility, tn:Transportobject .
+        ?s rdf:type mv:ParkingFacility, tn:Transportobject, adr:AdresseerbaarObject, dc:Location .
         ?s rdfs:label ?o .
-        ?s ext:voertuigenBeschikbaar ?cap .
-        ?s ext:totaalPlaatsen ?totCap .
         ?s geonames:nearby ?nearby .
-        ?s locn:geometry [ rdf:type geo:Point ; geo:long ?long ; geo:lat ?lat ] .
+        ?s trips:Transportobject.beschikbaarheid _:b .
+        ?s locn:geometry _:g .
+        _:b rdf:type trips:Beschikbaarheid ;
+            ext:totaalPlaatsen ?totCap ;
+            ext:voertuigenBeschikbaar ?cap ;
+            prov:generatedAtTime ?time .
+        _:e rdf:type geo:Point ; 
+            geo:long ?long ; 
+            geo:lat ?lat .
       } WHERE {
+        ?g prov:generatedAtTime ?time .
         GRAPH ?g {
           ?s rdf:type <http://schema.org/ParkingFacility> .
           ?s foaf:name ?o .
@@ -41,16 +52,20 @@ exports.cronjob = async () => {
 
   const writer = new N3.Writer(myFile, { end: false, prefixes:
     {
-      rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
-      rdfs: 'http://www.w3.org/2000/01/rdf-schema#',
+      adr:  'https://data.vlaanderen.be/ns/adres#',
+      dc:   'http://purl.org/dc/terms/',
+      ext:  'https://stijnbrysbaert.github.io/OSLO-extension/vocabulary.ttl#',
       foaf: 'http://xmlns.com/foaf/0.1/',
-      mv: 'http://schema.mobivoc.org/',
-      geo: 'http://www.w3.org/2003/01/geo/wgs84_pos#',
-      locn: 'http://www.w3.org/ns/locn#',
-      tn: 'https://data.vlaanderen.be/ns/transportnetwerk#',
-      ext: 'https://stijnbrysbaert.github.io/OSLO-extension/vocabulary.ttl#',
+      geo:  'http://www.w3.org/2003/01/geo/wgs84_pos#',
       geonames: 'http://www.geonames.org/ontology#',
-      xsd:      'http://www.w3.org/2001/XMLSchema#'
+      locn: 'http://www.w3.org/ns/locn#',
+      mv:   'http://schema.mobivoc.org/',
+      prov: 'http://www.w3.org/ns/prov#',
+      rdf:  'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+      rdfs: 'http://www.w3.org/2000/01/rdf-schema#',
+      tn:   'https://data.vlaanderen.be/ns/transportnetwerk#',
+      trips:'https://data.vlaanderen.be/ns/mobiliteit/trips-en-aanbod#',
+      xsd:  'http://www.w3.org/2001/XMLSchema#',
     }
   });
   const quads = await result.quads();
