46d193b354f150b5ddd641a0ff46746d8a071888
Stijn <stijn.brysbaert@ugent.be>
2020-10-19 09:31:25 +0200

    cross origin

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = 8080
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://query.linkeddatafragments.org"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 var options = {
     extensions: ['ttl'],
     index: 'output.ttl',
