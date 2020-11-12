47f9e1a7fa4bb3e4805aee67dcf26ec00ee5fa9d
Stijn <stijn.brysbaert@ugent.be>
2020-11-12 16:15:38 +0100

    Delete code to remove and create public dir

--- src/index.js
+++ src/index.js
@@ -4,15 +4,6 @@ const constants = require('../constants');
 const fs = require('fs');
 
 module.exports = async function() {
-    var dir = "./public";
-    try{
-        fs.rmdirSync(dir, {recursive: true});
-        console.log(dir, " deleted");
-        fs.mkdirSync(dir);
-        console.log(dir, " created");
-    }catch(err){
-        console.error(`Error while deleting ${dir}.`);
-    }
     bluebike.mapping();
     velo.mapping(constants.velo);
 }
