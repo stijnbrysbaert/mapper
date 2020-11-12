9a98a05879c0ed0cd8c94f5cf9ba59747bdb704c
Stijn <stijn.brysbaert@ugent.be>
2020-11-12 16:36:10 +0100

    Make ready for demo

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const cron = require('node-cron');
 const mapper = require('./src/index');
+const fs = require('fs');
 
 const app = express()
 const port = 8080
+const dir = "./public";
 
 //run query every hour
 cron.schedule('*/5 * * * *', function() {
+  if(!fs.existsSync(dir)){
+    try{
+      fs.mkdirSync(dir);
+    }catch(err){
+      console.error("Error while creating folder %s", dir);
+    }
+  }
   mapper();
 });
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 const bluebike = require('./bluebike.js');
 const velo = require('./velo');
 const constants = require('../constants');
-const fs = require('fs');
 
 module.exports = async function() {
     bluebike.mapping();
-    velo.mapping(constants.velo);
+    // velo.mapping(constants.velo);
 }
