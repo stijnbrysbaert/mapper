07dbe2ee148ee1062c957bb006a7e66b4a394aa0
Stijn <stijn.brysbaert@ugent.be>
2020-11-12 15:27:19 +0100

    Remove and make public dir everytime

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 const bluebike = require('./bluebike.js');
 const velo = require('./velo');
 const constants = require('../constants');
+const fs = require('fs');
 
 module.exports = async function() {
+    var dir = "./public";
+    try{
+        fs.rmdirSync(dir, {recursive: true});
+        console.log(dir, " deleted");
+        fs.mkdirSync(dir);
+        console.log(dir, " created");
+    }catch(err){
+        console.error(`Error while deleting ${dir}.`);
+    }
     bluebike.mapping();
     velo.mapping(constants.velo);
 }
