57125c007a0ea29160196bfc248ece735157e3d5
Stijn <stijn.brysbaert@ugent.be>
2020-11-17 15:21:24 +0100

    Refactor api call velo

--- constants.js
+++ constants.js
@@ -7,7 +7,6 @@ module.exports = {
     velo: {
         get_entities: "https://ext-api-gw-p.antwerpen.be/digipolis/sabhdsveloa/v1/entities",
         rml: "./resources/velo.rml.ttl",   
-        json: "./resources/velo.json",
         ld: "./public/velo.ttl",
         yarrrml: "./mappings/mapping_velo.yml"
     },
--- src/velo.js
+++ src/velo.js
@@ -5,36 +5,47 @@ const fs = require('fs');
 const path = require("path");
 const N3 = require('n3');
 const velo = require('../queries/query_velo');
+const getVelos = require('./api/velo');
     
-function yarrrmlParser (operator, next){
-        console.log("start parser");
-        const y2r = new yarrrml();
-        const file = fs.readFileSync(path.resolve(operator.yarrrml), 'utf8');
-        const triples = y2r.convert(file);
-        var output = fs.createWriteStream(operator.rml, {emitClose: true});
-        const writer = new N3.Writer(output, {prefixes: velo.prefixes});
-        writer.addQuads(triples);
-        output.on('error', function (err) {
-                console.log(err);
+function yarrrmlParser (jsonVelo){
+        return new Promise((resolve, reject) => {
+                const y2r = new yarrrml();
+                const file = fs.readFileSync(path.resolve(constants.velo.yarrrml), 'utf8');
+                const triples = y2r.convert(file);
+                var output = fs.createWriteStream(constants.velo.rml, {emitClose: true});
+                const writer = new N3.Writer(output, {prefixes: velo.prefixes});
+                writer.addQuads(triples);
+                output.on('error', function (err) {
+                        throw "error in yarrrml parser";
+                });
+                writer.end(() => {resolve(jsonVelo)});
+                return;
         });
-        writer.end(() => {next(operator)});
+        
 }
 
-async function rmlMapper(operator){
-        const rmlmapperPath = './rmlmapper.jar';
-        const tempFolderPath = './tmp';
-
-        const wrapper = new RMLMapperWrapper(rmlmapperPath, tempFolderPath, true);
-        const rml = fs.readFileSync(operator.rml, 'utf-8');
-        const sources = {
-        'velo.json': fs.readFileSync(operator.json, 'utf-8')
-        };
-        const result = await wrapper.execute(rml, {sources, generateMetadata: false, serialization: 'turtle'});
-        const file = fs.createWriteStream(constants.velo.ld);
-        file.write(result.output);
-        console.log("mapped velos %s", new Date());
+async function rmlMapper(jsonVelo){
+        return new Promise(async (resolve, reject) => {
+                const rmlmapperPath = './rmlmapper.jar';
+                const tempFolderPath = './tmp';
+        
+                const wrapper = new RMLMapperWrapper(rmlmapperPath, tempFolderPath, true);
+                const rml = fs.readFileSync(constants.velo.rml, 'utf-8');
+                const sources = {
+                'velo.json': JSON.stringify(jsonVelo)
+                };
+                const result = await wrapper.execute(rml, {sources, generateMetadata: false, serialization: 'turtle'});
+                const file = fs.createWriteStream(constants.velo.ld);
+                file.write(result.output);
+                console.log("mapped velos %s", new Date());
+        });
 }
 
-exports.mapping = async (operator) => {
-        yarrrmlParser(operator, rmlMapper);
+exports.mapping = async () => {
+        (new Promise(getVelos))
+                .then(yarrrmlParser, (e) => {console.error("hey", e)})
+                .then(rmlMapper)
+                .catch((e) => {
+                        console.error(e);
+                });
 }
